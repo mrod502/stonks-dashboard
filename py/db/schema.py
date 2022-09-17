b10249de5279e603229b402260a9094a45ed66eb
Michael Rodriguez <mike@recursivelabs.org>
2022-09-17 14:48:13 -0400

    upgrade deps

--- py/db/schema.py
+++ py/db/schema.py
@@ -2,24 +2,15 @@ from typing import Union
 from enum import Enum
 
 
-
-
-
-
 class Schema:
-  def __init__(self, fields:dict):
-    self.fields = fields
-    pass
-    
-  def serialize(self,encoding:str="msgpack")->Union(str,bytes):
-    if encoding=="msgpack":
-      return ""
-    if encoding == "json":
-      return ""
-    
-    return ""
+    def __init__(self, fields: dict):
+        self.fields = fields
+        pass
 
-  
+    def serialize(self, encoding: str = "msgpack") -> Union(str, bytes):
+        if encoding == "msgpack":
+            return ""
+        if encoding == "json":
+            return ""
 
-
-    
+        return ""
